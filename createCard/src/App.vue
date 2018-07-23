4e9c1d96e9295e6902cab3371065685f854bd7cf
watsonLidaye <610466535@qq.com>
2018-07-23 10:10:50 +0800

    项目初始化配置

---
+++ createCard/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import './js/public.js'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import "./css/common.css";
+@import "./css/public.css";
+
+</style>
